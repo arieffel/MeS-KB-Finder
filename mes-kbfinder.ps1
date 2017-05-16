bd55db5206e7ece8de8c50f3d6db2c585db03456
arieffel <arieffel@merchante-solutions.com>
2017-05-16 11:17:54 -0400

    Update mes-kbfinder.ps1

--- mes-kbfinder.ps1
+++ mes-kbfinder.ps1
@@ -1,4 +1,3 @@
-
 cls
 $username = $env:USERNAME
 $computername = $env:COMPUTERNAME
@@ -7,22 +6,19 @@ $patched = 1
 $hotfixes = @("KB4012212", "KB4012213", "KB4012214", "KB4012215", "KB4012216", "KB4012217", "KB4012598", "KB4013429", "KB4015217", "KB4015438", "KB4015549", "KB4015550", "KB4015551", "KB4015552", "KB4015553", "KB4016635", "KB4019215", "KB4019216", "KB4019264", "KB4019472")
 
 write-host "================================================`n"
-write-host "     Merchant e-Solutions Hotfix Check Tool      "
-write-host "           Created by: Alex Rieffel`n              "
-write-host "    Email: arieffel@merchante-solutions.com      `n"
+write-host "     Merchant e-Solutions Hotfix Check Tool       "
+write-host "           Created by: Alex Rieffel`n             "
+write-host "    Email: arieffel@merchante-solutions.com     `n"
 write-host "================================================`n"
 
 $hotfix = Get-HotFix | Where-Object {$hotfixes -contains $_.HotfixID} | Select-Object -property “HotfixID”
+
 if (Get-HotFix | Where-Object {$hotfixes -contains $_.HotfixID}) {
-$ID = $hotfix.HotfixID
 $patched = 0
 }
     
 if (($patched -eq 0) -or ($WindowsVersion = 1703)){
-write-host "Your machine is patched.  No further action is needed`n" -ForegroundColor Green
-    if ($patched -eq 0) {
-    write-host "The script found the following hotfix: $ID" -ForegroundColor Green
-    }
+    write-host "Your machine is patched.  No further action is needed`n`n`n" -ForegroundColor Green
 }
 else {
     write-host ("YOUR MACHINE IS NOT PATCHED!`n") -ForegroundColor Red
@@ -32,6 +28,5 @@ else {
     write-host "Windows Version: $WindowsVersion`n`n"
     write-host "DO NOT ATTEMPT TO CONNECT TO CORPORATE VPN UNTIL YOUR MACHINE HAS BEEN SUCCESSFULLY REMEDIATED!`n`n"
 }
-
 
 pause
